c3607df80e8f0d1f2254d4acd0145c4ac397fa53
SAEBOMPAK <saebompak@gmail.com>
2021-06-20 08:11:52 +0900

    feat: add swiper

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ function changeCircleColor(color){
   const circle = document.querySelector('.circle');
   circle.style.background = color;
 }
+
+var swiper = new Swiper(".mySwiper", {
+  direction: "vertical",
+  slidesPerView: 1,
+  // spaceBetween: 30,
+  mousewheel: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
