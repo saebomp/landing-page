dd4fcc9356054dbff6c3c3bc26088e7e4b312826
SAEBOMPAK <saebompak@gmail.com>
2021-07-22 01:08:03 +0900

    fix: function for modal

--- script.js
+++ script.js
@@ -54,20 +54,19 @@ activeMenus.forEach(activeMenu => {
 /******************************************************/
 /**************************modal***********************/
 /*****************************************************/
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+myBtn.addEventListener('click', () => {
+  myModal.style.display = "block";
   }
-}
+)
+span.addEventListener('click', () => {
+  myModal.style.display = "none";
+  }
+) 
+window.addEventListener('click', () => {
+  if (e.target == myModal) {
+    myModal.style.display = "none";
+  }
+  }
+)
